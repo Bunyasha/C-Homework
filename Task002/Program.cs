0cdcda891b2310b1e66250f30266b5415fd140a3
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-05 22:07:53 +0400

    Добавила папку с решением второй задачи

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное из трех чисел
+int firstNumber = new Random().Next(1, 100);
+Console.WriteLine($"Первое число: {firstNumber} ");
+
+int randomizer = new Random().Next(1, 100);
+
+int secondNumber = randomizer;
+Console.WriteLine($"Второе число: {secondNumber} ");
+
+int thirdNumber = new Random().Next(1, 100);
+Console.WriteLine($"Третье число: {thirdNumber} ");
+
+int max = firstNumber;
+
+if (max < secondNumber) max = secondNumber;
+if (max < thirdNumber) max = thirdNumber;
+Console.WriteLine($"Наибольшее из трех чисел -  {max} ");
