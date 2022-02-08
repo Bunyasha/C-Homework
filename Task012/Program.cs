d39e3c21add3a5b9bb271e3745f7c25c33ac908a
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-08 21:21:20 +0400

    Добавила папку с решением задачи номер 12

---
+++ Task012/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int GetRandom()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int randomizer = GetRandom();
+Console.WriteLine($"Сгенерированное число - {randomizer}");
+int result1 = randomizer / 100;
+int result2 = randomizer % 10;
+Console.WriteLine($"Число с удаленной второй цифрой - {result1}{result2}");
