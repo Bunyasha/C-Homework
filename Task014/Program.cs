657b125b92b4cc4b70bde9e32ace648a35d32588
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-08 21:41:51 +0400

    Добавила папку с решением 14 задачи

---
+++ Task014/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int GetRandom()
+{
+    int number = new Random().Next(1, 1000);
+    return number;
+}
+int randomizer = GetRandom();
+Console.WriteLine($"Сгенерированное число - {randomizer}");
+if (randomizer > 100 || randomizer == 100)
+{
+    int result = randomizer % 10;
+    Console.WriteLine($"Третья цифра числа - {result}");
+}
+else
+{
+    Console.WriteLine($"Число состоит из двух цифр");
+}
