4ac20c6622f3858a2a69afd548f962ec862e7999
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-09 22:01:38 +0400

    Добавила папку с решением задачи 24

---
+++ Task024/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N (максимальное число для возведения в куб): ");
+string numberN = Console.ReadLine();
+int n = int.Parse(numberN);
+
+Console.WriteLine($"Таблица кубов от 1 до указанного Вами числа:");
+
+int value = 1;
+int power = 3;
+while (value <= n)
+{
+    Console.WriteLine($"{value}^{power} = {value * value * value}");
+    value++;
+}
+
+
+
+
+
