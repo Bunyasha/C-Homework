bcd8714a31c3bf43eead496024d9003826b92081
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-14 20:42:29 +0400

    Добавлена папка с решением 18 задачи

---
+++ Task018/Program.cs
@@ -0,0 +1,44 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.Write($"¬(X ⋁ Y) = ¬X ⋀ ¬Y");
+
+bool X = false;
+bool Y = true;
+
+if (!(X || Y) == (!X) && (!Y));
+Console.WriteLine("При Х = false и Y = true утвержение - истинно" );
+else
+{
+Console.WriteLine("При Х = true и Y = true утвержение - ложно" );
+}
+bool X = false;
+bool Y = false;
+
+if (!(X || Y) == (!X) && (!Y));
+Console.WriteLine("При Х = false и Y = false утвержение - истинно" ); 
+else
+{
+Console.WriteLine("При Х = false и Y = false утвержение - ложно" );
+}
+    
+
+bool X = true;
+bool Y = true;
+
+if (!(X || Y) == (!X) && (!Y));
+Console.WriteLine("При Х = true и Y = true утвержение - истинно" );
+else
+{
+Console.WriteLine("При Х = true и Y = true утвержение - ложно" );
+}
+    
+
+bool X = false;
+bool Y = true;
+
+if (!(X || Y) == (!X) && (!Y));
+Console.WriteLine("При Х = true и Y = false утвержение - истинно" );
+else
+{
+Console.WriteLine("При Х = true и Y = true утвержение - ложно" );
+}
