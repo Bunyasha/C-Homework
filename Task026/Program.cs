ddb049583a57ae55fb10fa5e08621234d1e78f6e
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-15 21:42:09 +0400

    Добавлена задача 26

---
+++ Task026/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число (A), которое вы хотели бы возводить в степень (B): ");
+string numberA = Console.ReadLine();
+int A = int.Parse(numberA);
+
+Console.Write("Введите число B (степень числа А): ");
+string numberB = Console.ReadLine();
+int B = int.Parse(numberB);
+
+int pow = 1;
+for (int i = 0; i< B; i++)
+{
+    pow *= A;
+}
+Console.WriteLine($"{A} в степени {B} = {pow}");
