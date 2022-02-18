cd55b5fe6bf0f07476e1d7a356610bedb4d48ff5
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-18 21:37:34 +0400

    Добавлено решение задачи 42

---
+++ Task042/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите колличество чисел: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < number; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
