b99baaad40cdd79c6c9974e185099535610f7f8a
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-22 22:26:12 +0400

    Добавлено решение задачи 46

---
+++ Task046/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу масштабирования фигуры
+
+string strValue = "(0,0) (2,0) (2,2) (0,2)";
+Console.WriteLine("Введите множитель: ");
+int k = int.Parse(Console.ReadLine());
+
+strValue = strValue.Replace("(", "").Replace(")", "").Replace(",", " ");
+
+// разбиваем строку на массив. разделитель в данном случае - пробел.
+string[] strArray = strValue.Split(" ");
+
+string result = string.Empty;
+
+foreach (var item in strArray)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < strArray.Length - 1; i = i + 2)
+{
+    int x = k * int.Parse(strArray[i]);
+    int y = k * int.Parse(strArray[i + 1]);
+
+    Console.WriteLine($"X = {x}, Y = {y}");
+
+    result = result + $"({x}, {y}) ";
+}
+
+Console.WriteLine("_________________");
+Console.WriteLine(result);
