bad94a2169e4ac41f9948ecf21c4078c7a463546
Elena Bunyakova <elenijacinte9@gmail.com>
2022-02-26 22:02:09 +0400

    Добавлено решение задачи 52

---
+++ Task052/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52: В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты.
+
+int[,] array = new int[5, 5];
+int[,] matrix = new int[5, 5];
+
+void SquaredEvenIndex()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = array[i, j] * array[i, j];
+            }
+            else{
+                matrix[i,j] = array [i,j];
+            }
+        }
+    }
+}
+
+void FillArray()
+{
+    Random number = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = number.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+FillArray();
+PrintArray(array);
+SquaredEvenIndex();
+Console.WriteLine();
+PrintArray(matrix);
+
