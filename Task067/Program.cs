b1217ee9090e74903a03c1c6388f9f7221165b5f
Elena Bunyakova <elenijacinte9@gmail.com>
2022-03-12 22:11:53 +0400

    решена задача 67

---
+++ Task067/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: 
+//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите натуральное число (значение M): ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Введите натуральное число (значение N): ");
+int numberN = int.Parse(Console.ReadLine());
+
+string NumbersRec(int numberM, int numberN)
+{
+    if (numberM <= numberN) return $"{numberM} " + NumbersRec(numberM + 1, numberN);
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersRec(numberM, numberN));
